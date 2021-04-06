240ab8118f5e40433b45e41f5b21f7ca5b939736
kkretschmerhensley <k.kretschmerhensley@gmail.com>
2021-04-06 17:51:44 -0500

    copy/pasted and reformatting code from book

---
+++ src/Protected.js
@@ -0,0 +1,24 @@
+import React, { useEffect } from 'react';
+import { Auth } from 'aws-amplify';
+import Container from './Container';
+
+const Protected = ({history}) => {
+  useEffect(() => {
+    Auth.currentAuthenticatedUser()
+      .then(() => {
+          console.log("User was authenticated");
+        }
+      )
+      .catch(() => {
+        history.push('/profile');
+      })
+  }, []);
+
+  return (
+    <Container>
+      <h1>Protected route</h1>
+    </Container>
+  );
+}
+
+export default Protected;
